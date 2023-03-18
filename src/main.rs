50cdbbe716305ea5f203d6b9327c80ef1621a67a
zzjcarrot <10383529+zzjcarrot@user.noreply.gitee.com>
2023-03-18 21:29:22 +0800

    寄存器OK

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    use virtual_machine::mem::Memory;
-    let mut mem = Memory::new(1);
-    mem.write_u8(0,10);
-    println!("{}",mem.read_u8(0));
-    println!("{}",mem.read_u16(0));
-    println!("{}",mem.read_u32(0));
-    println!("{}",mem.read_u64(0));
+    use virtual_machine::reg::Reg;
+    let mut buffer:[u8;8] = [0;8];
+    let mut reg = Reg::new(&mut buffer,8);
+    reg.write(1024);
+    for i in 0..8 {
+        print!("{}",reg.read_byte(i));
+    }
+    println!("\n{}",reg.read());
 }
--- src/reg.rs
+++ src/reg.rs
@@ -1,1 +1,40 @@
-struct
+pub struct Reg<'a>{
+    data:&'a mut [u8],
+    size:usize,
+}
+
+impl Reg<'_> {
+    pub fn new(data:&'_ mut [u8], size:usize) -> Reg{
+        Reg{data, size}
+    }
+
+    pub fn read(&self) -> u64{
+        let mut bytes:u64 = 0;
+        for i in 0..self.size{
+            bytes <<= 8;
+            bytes += self.data[i] as u64;
+        }
+        bytes
+    }
+    pub fn read_byte(&self,index:usize) -> u8{
+        if index > self.size {
+            return 0;
+        }
+        self.data[index]
+    }
+
+    pub fn write_byte(&mut self, index:usize, value:u8){
+        self.data[index] = value;
+    }
+
+    pub fn write(&mut self,value:u64){
+        for i in 0..self.size{
+            self.data[i] = (value >> ((self.size - i - 1) * 8)) as u8;
+        }
+    }
+
+    pub fn get_size(&self) -> usize{
+        self.size
+    }
+}
+
